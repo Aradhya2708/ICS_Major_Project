fbd63221e569fc6772caaeca54a958b32fdf5e43
Aradhya2708 <147337970+Aradhya2708@users.noreply.github.com>
2024-04-24 21:25:19 +0530

    Uploaded Files

---
+++ ICS_Project.h
@@ -0,0 +1,342 @@
+#ifndef ICS_PROJECT_H
+#define ICS_PROJECT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "cJSON.h"
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <signal.h>
+#include "ICS_Project.h"
+#include <math.h>
+// #include"cJSON.c"
+#include<time.h>
+#include <stdarg.h>
+
+
+#define MAX_PLAYER_ID_LENGTH 100
+
+#define BLK 30 //Black
+#define RED 31 //Red
+#define GRN 32 //Green
+#define YEL 33 //Yellow
+#define BLU 34 //Blue
+#define MAG 35 //Magenta
+#define CYN 36 //Cyan
+#define WHT 37 //White
+
+#define REGU 0 //Regular
+#define BOLD 1 //Bold
+#define UNDL 4 //Underlined
+#define HIB 0  //High Intensity Background
+#define HIT 0  //High Intensity Text
+#define BHT 1  //Bold High Intensity Text
+
+#define LOW 20
+#define MED 50
+#define HIG 100
+//Structures
+
+// Define the Inventory structure
+typedef struct
+{
+    int size;     // Size of the inventory
+    char **items; // Array of items in the inventory
+    int *activeItems;
+} Inventory;
+
+// Define Stats structure
+typedef struct
+{
+    int HP;
+    int atk;
+    int def;
+    int agi;
+    int str;
+    int dex;
+    int intel;
+    int luck;
+} Stats;
+
+// Define the Player structure
+typedef struct
+{
+    char *id;
+    char *name;            // Player's name
+    int level;             // Player's level
+    int wtdLevel;          // Wantedted level
+    int xp;                // Player's experience points
+    int gold;              // Player's gold
+    char *currentLocation; // Player's current location [Change this]
+    Stats *stats;          // Player's stats (e.g., HP, attack, defense)
+    Inventory *inventory;  // Player's inventory
+    int **NPCInfo;         // Player's interactions with NPCs (and status of Quest-line)
+    char **activeQuests;   // Player's active quests
+} Player;
+
+// Define a typedef for function pointer
+typedef int (*GameFunction)();
+
+// Define a struct to hold string-function pairs
+typedef struct {
+    const char *str;
+    GameFunction func;
+} StringFunctionPair;
+
+
+typedef enum
+{
+    SOLDIER,
+    SHOPKEEPER,
+    FIGHTER,
+    CITIZEN,
+    ///Add more as needed
+}TypeOfNpc;
+
+typedef enum
+{
+    WEAPON,
+    ARMOR,
+    POTION,
+    QUEST_ITEM,
+    RING,
+    CONSUMABLE,
+// ADD MORE AS NEEDED
+
+}Itemtype;
+
+typedef enum {
+    NO_BUFF,
+    ATT_INC,
+    ATT_DEC,
+    DEF_INC,
+    DEF_DEC,
+    HP_INC,
+    HP_DEC,
+    AGI_INC,
+    AGI_DEC,
+    STR_INC,
+    STR_DEC,
+    DEX_INC,
+    DEX_DEC,
+    INT_INC,
+    INT_DEC,
+    LUCK_INC,
+    LUCK_DEC,
+    // Add more buffs and debuffs as needed
+} TypeOfBuff;
+
+typedef struct {
+    TypeOfBuff effect;
+    int duration;
+    int valueType; // 0 for direct value change, 1 for percentage change
+    int type;      //ACTIVE(0) OR PASSIVE(1)
+    int value;
+} BUFF;
+
+typedef enum
+{
+    COMMON,
+    UNCOMMON,
+    RARE,
+    UNIQUE,
+    LEGENDARY,
+    MYTHICAL,
+// ADD MORE AS NEEDED
+}Grade;
+
+typedef struct 
+{
+    char *name;  //Use strdup
+    char *description;
+    int durability;
+    Grade grade;
+    BUFF buff;
+    Itemtype type;
+}Item;
+
+typedef enum
+{
+    QUEST_LVL,
+    QUEST_STATUS,
+    RELATONSHIP,
+}QuestManager;
+
+typedef struct
+{
+    int xp;
+    int gold;
+    BUFF buff;
+}Reward;
+
+
+void MouseControl(int *x, int *y);
+int return_current_status(char guessed_string[], char *current_status[], char selected_word[], int length);
+void print_current_status(char *current_status[], int length);
+void display_hangman(int result);
+void intro();
+int start_hangman();
+int Hangtheman();
+
+
+int Snakegame();
+
+void genField(int size, int **field);
+void reveal(int size, int x, int y, int *field, int *revealed);
+void flag(int size, int x, int y, int *field, int *revealed, int *flag_count);
+void printReveal(int size, int *revealed, int *field, int flag_mode);
+int yesnoMine(int x, int y, int *field, int size);
+int numRevealed(int size, int *revealed);
+void cheat(int size, int *field);
+int playMinesweeper(int size);
+int Minesweeper();
+
+void swap(int *a, int *b);
+void shuffle_array(int arr[], int n);
+void genMat(int size, int num, int mat[size][size]);
+void printMemory1(int size, int num, int mat[size][size], int prog_arr[num]);
+void printMemory2(int size, int num, int mat[size][size], int prog_arr[num], int click1_x, int click1_y, int click2_x, int click2_y);
+void gameMemory(int size, int num, int mat[size][size], int *attempts, int prog_arr[num], int *progress);
+int playMemory(int size, int num, int attempts);
+int start_memory();
+int Memory();
+
+typedef struct cell
+{
+    int x, y;
+} cell_pos;
+
+
+void print_prison(int size, int i, int j, int k);
+void printPrisonEscape(int size, int prison[2 * size][2 * size], cell_pos exit, cell_pos pl_pos, cell_pos guard_pos);
+void result(int size, int prison[2 * size][2 * size], cell_pos exit, cell_pos pl_pos, cell_pos guard_pos);
+void movePrison(int size, int prison[2 * size][2 * size], cell_pos *pl_pos, char pl_move, cell_pos exit);
+void playermove(int size, int prison[2 * size][2 * size], cell_pos *pl_pos, cell_pos guard_pos, cell_pos exit);
+void guardmove(int size, int prison[2 * size][2 * size], cell_pos *pl_pos, cell_pos *guard_pos, cell_pos exit);
+void makePrison(int size, int prison[2 * size][2 * size]);
+int playPrisonEscape(int size, int map);
+int start_prison_escape();
+int PrisonEscape();
+
+int dig(int n, int num, int i);
+void wordlegame(int n, int num, int guess);
+int playWordle(int n, int attempts);
+int start_wordle();
+int Lockpicking();
+
+void printMessage(int x, int y, char message);
+void printFalcon(int x, int y);
+void deleteFalcon(int x, int y);
+void flyFalcon(int *falcon_x, int *falcon_y, int *prev_flight);
+void printScopeFalcon(int x, int y, char c);
+void openScopeFalcon(int x, int y, int side);
+void deleteScopeFalcon(int x, int y, int side);
+int playFalcon();
+int falconry();
+
+void print_combat(int plStamina, int oppStamina);
+void gameCombat(int plWeapon, int *plStamina, int oppWeapon, int *oppStamina, int pl_att, int pl_def, int opp_att, int opp_def);
+int playCombat(int plWeapon, int plStamina, int oppWeapon, int oppStamina);
+int start_combat();
+int Combat();
+//From world.h
+
+
+void delay2(int milliseconds);
+
+void printStory(const char *sentence,int color,int style,int time);
+
+void printFormattedStringWithColorAndDelay(const char *format, int color,int style, int delay, ...);
+
+Player* gameInitializer(char *PlayerID);
+
+void selectState(int *state);
+
+void processState(Player *player,int *state);
+
+
+
+
+//From player.h
+
+// Function to create a new player
+Player *createNewPlayer(char *playerID);
+
+void savePlayerData(Player *player);
+
+Player *loadPlayerData(char *playerID);
+
+int playMinigame(Player *player,char *gameName);
+
+void freePlayer(Player *player);
+
+
+//From characters.h
+
+int getNpcNumber();
+int getNPCID(char *npcName);
+void interactWith(Player *player, char *npc); // Requires Quest Submission
+void chooseNPC(char **NPCsAvailable, Player *player,int *state);
+
+
+
+
+//From item.h
+
+// Function prototypes
+
+// void createItem(Item *item,const char name[],const char desc[],Grade grade,Itemtype itemtype,TypeOfBuff effect,int duration,int valueType,int type,int value);
+// void modifyItem(Item *item,const char name[],const char desc[],Grade grade,Itemtype itemtype,TypeOfBuff effect,int duration,int valueType,int type,int value);
+
+// void inspectItem(Item item);//generates item data
+// void useItem(Player *player,Item *item);
+// void destroyItem(Item *item);
+
+// void serializeItem(Item item, FILE *file);
+// void deserializeItem(Item *item, FILE *file);
+
+
+
+
+
+//From locations.h
+
+void navigationMode(Player *player,int *state);
+char *getQuestLocation(char *NPCQuestID);
+char **returnNPCsAvailable(char *locationNode);
+
+
+
+
+//From quest.h
+
+void questMode(Player *player,int *state);
+void addActiveQuest(Player *player, char *questID);
+void activateQuest(Player *player, char *npc, char *NPCQuestID);
+char *getQuestDescription(char *NPCQuestID);
+void giveQuestReward(Player *player, char *questID);
+bool anyQuesttoSubmit(Player *player, char *npcName);
+char *getQuestID(Player *player, char *npcName);
+char *questDialogues(char *questID);
+void interactionMode(Player* player,int *state);
+
+
+
+
+
+
+
+
+//From story.h
+
+
+
+
+//From minigame
+
+#endif
---
+++ b23cs1032_b23ch1008_b23ci1012_b23mt1019_main.c
@@ -0,0 +1,61 @@
+#include "ICS_Project.h"
+// #include"cJSON.c"
+
+
+int state;
+
+int main()
+{
+    printf("\e[1;1H\e[2J");
+
+    // printStory("")
+
+    char *playerID = (char *)malloc(sizeof(char) * MAX_PLAYER_ID_LENGTH);
+    if (playerID == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return EXIT_FAILURE;
+    }
+
+    Player *player = NULL;
+
+    // printRules();
+
+    printStory("\nEnter Login ID : ",YEL,BOLD,HIG);
+    if (scanf("%s", playerID) != 1)
+    {
+        fprintf(stderr, "Error reading player ID\n");
+        free(playerID);
+        return EXIT_FAILURE;
+    }
+
+    player = gameInitializer(playerID);
+    if (player == NULL)
+    {
+        fprintf(stderr, "Failed to initialize player\n");
+        free(playerID);
+        return EXIT_FAILURE;
+    }
+    // printStory("\nid-%s",player->id,31,0);
+    // printf("\nname-%s",player->name);
+    // printf("\nlevel-%d",player->level);
+    // printf("\nstats,hp-%d",player->stats->HP);
+    // printf("\ninventory,items[0]-%s",player->inventory->items[0]);
+    // printf("\ninventory,aitems[0]-%d",player->inventory->activeItems[0]);
+    // printf("\ngold-%d",player->gold);
+    // printf("\ncur_loc-%s",player->currentLocation);
+    // printf("\nact_que[0]-%s",player->activeQuests[0]);
+    // printf("\nnpcInfo[0][0]-%d",player->NPCInfo[0][0]);
+//     printPrologue();
+
+    selectState(&state); // function that can be called from anywhere by player to choose a state
+    while (state >= 0)
+        processState(player, &state); // function that processes the state
+
+    printStory("\n\nThank you for playing The Vindication ",YEL,BOLD,HIG); /// EXIT STATEMENT
+
+    savePlayerData(player);
+    // Cleanup
+    freePlayer(player);
+    return EXIT_SUCCESS;
+}
